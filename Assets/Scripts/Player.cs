553f9fd45a69e9ca5d235553591849cf7de7e686
Shubham Gupta <shubham0209@gmail.com>
2018-09-11 03:12:58 +0530

    Added player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+	}
+
+}
