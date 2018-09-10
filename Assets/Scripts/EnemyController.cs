63c3a455f551d4e9f3853a4f0c75c102c38ce997
Shubham Gupta <shubham0209@gmail.com>
2018-09-11 02:53:10 +0530

    Removed redundant array

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,18 +5,14 @@ public class EnemyController : MonoBehaviour {
 	public Transform player;
 	public GameObject enemyPrefab;
 	public float randomPlace;
-	[@HideInInspector]
-	public Transform[] enemies;
 
 	public void CreateEnemies (int n) {
-		enemies = new Transform[n];
 		for(int i = 0; i < n; i++){
 			GameObject enemy = Instantiate(enemyPrefab);
 			enemy.transform.position = transform.position + new Vector3(Random.value * randomPlace - randomPlace/2, 0, Random.value * randomPlace - randomPlace/2);
 			Enemy script = enemy.GetComponent<Enemy>();
 			script.controller = this;
 			script.destination = player;
-			enemies[i] = enemy.transform;
 		}
 	}
 
